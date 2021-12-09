382454ff87c9dedb1a0f203ce86f44788b769c8e
Artem <artm-porjad@mail.ru>
2021-12-09 20:04:42 +0300

    1 practicheskaya

--- Practicheskaya1.py
+++ Practicheskaya1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
-dt = pd.read_csv("./AustralianAverageMaxTemp.csv")
+dt = pd.read_csv("./Tempreture_1901_2016_Pakistan -.csv")
 X = dt["Year"].values.reshape(-1,1)
 y = dt["Temp"].values.reshape(-1,1)
 
@@ -13,7 +13,7 @@ lin_reg.fit(X,y)
 plt.scatter(X,y)
 y_pred = lin_reg.predict(X)
 plt.plot(X, y_pred, color="red")
-plt.title("Температура Австралии в мае 1975-2019")
+plt.title("Температура Афганистана в апреле 1950-2016")
 plt.xlabel('Год')
 plt.ylabel('Температура')
 plt.show()
@@ -28,7 +28,7 @@ X_grid = np.arange(min(X),max(X),0.1)
 X_grid = X_grid.reshape(len(X_grid),1)
 plt.scatter(X,y)
 plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)))
-plt.title("Температура Австралии в мае 1975-2019")
+plt.title("Температура Афганистана в апреле 1950-2016")
 plt.xlabel('Год')
 plt.ylabel('Температура')
 plt.show()
